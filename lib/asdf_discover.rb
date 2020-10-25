f8efd17234cf6db115d4fa7fd38eb72403893972
Jason Schweier <jason.schweier@gmail.com>
2020-10-25 17:37:52 -0400

    Register searchers

--- lib/asdf_discover.rb
+++ lib/asdf_discover.rb
@@ -3,4 +3,17 @@ require "asdf_discover/searchers/dot_ruby_version"
 require "asdf_discover/version"
 
 module AsdfDiscover
+  def self.add_searcher(*searchers)
+    @searchers ||= []
+    @searchers.concat(searchers)
+  end
+
+  def self.search
+    @searchers
+      .map(&:new)
+      .map(&:call)
+      .select(&:found?)
+  end
+
+  add_searcher Searchers::DotRubyVersion
 end
