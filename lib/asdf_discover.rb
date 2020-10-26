afbd0f4a8e68c7ddb8ece7c115cae6b22f879902
Jason Schweier <jason.schweier@gmail.com>
2020-10-26 19:16:40 -0400

    Exit from exe when .tool-versions already exists

--- lib/asdf_discover.rb
+++ lib/asdf_discover.rb
@@ -3,6 +3,8 @@ require "asdf_discover/searchers/dot_ruby_version"
 require "asdf_discover/version"
 
 module AsdfDiscover
+  TOOL_VERSIONS = ".tool-versions"
+
   def self.add_searcher(*searchers)
     @searchers ||= []
     @searchers.concat(searchers)
